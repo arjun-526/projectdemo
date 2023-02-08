703fe88fda7f867ee99c2e693b4be461218e34d7
arjun-526 <52605181+arjun-526@users.noreply.github.com>
2023-02-08 22:34:27 +0530

    updated mapping method

--- src/main/java/com/example/MyService.java
+++ src/main/java/com/example/MyService.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyService {
 
-	@RequestMapping("/Hello")
+	@GetMapping("/Hello")
 	public String hello()
 	{
 		return "Hello";
